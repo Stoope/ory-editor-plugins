27e936d0ab409e2244382e9f1746e61e832005a7
Stepan Kurennykh <stoope@outlook.com>
2018-07-08 00:10:09 +0300

    fix mui prefix

--- src/BottomToolbar/index.js
+++ src/BottomToolbar/index.js
@@ -1,6 +1,16 @@
 import React from "react";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  createGenerateClassName
+} from "@material-ui/core/styles";
+import JssProvider from "react-jss/lib/JssProvider";
 import Snackbar from "@material-ui/core/Snackbar";
+
+const generateClassName = createGenerateClassName({
+  dangerouslyUseGlobalCSS: true,
+  productionPrefix: "oep"
+});
 
 const theme = createMuiTheme({
   palette: {
@@ -15,29 +25,31 @@ const childStyle = {
 };
 
 const BottomToolbar = ({ open = false, children }) => (
-  <Snackbar open={open}>
-    <div
-      style={{
-        border: "1px solid rgba(0, 0, 0, 0.87)",
-        borderRadius: "4px 4px 0px",
-        backgroundColor: "rgba(0, 0, 0, 0.87)",
-        padding: "12px 24px",
-        minWidth: "600px",
-        flexDirection: "column",
-        display: "flex"
-      }}
-    >
-      <MuiThemeProvider theme={theme}>
-        {children && Array.isArray(children) ? (
-          children
-            .filter(item => item)
-            .map(item => <div style={childStyle}>{item}</div>)
-        ) : (
-          <div style={childStyle}>{children}</div>
-        )}
-      </MuiThemeProvider>
-    </div>
-  </Snackbar>
+  <JssProvider generateClassName={generateClassName}>
+    <Snackbar open={open}>
+      <div
+        style={{
+          border: "1px solid rgba(0, 0, 0, 0.87)",
+          borderRadius: "4px 4px 0px",
+          backgroundColor: "rgba(0, 0, 0, 0.87)",
+          padding: "12px 24px",
+          minWidth: "600px",
+          flexDirection: "column",
+          display: "flex"
+        }}
+      >
+        <MuiThemeProvider theme={theme}>
+          {children && Array.isArray(children) ? (
+            children
+              .filter(item => item)
+              .map(item => <div style={childStyle}>{item}</div>)
+          ) : (
+            <div style={childStyle}>{children}</div>
+          )}
+        </MuiThemeProvider>
+      </div>
+    </Snackbar>
+  </JssProvider>
 );
 
 export default BottomToolbar;
